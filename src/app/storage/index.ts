501257ec0bff371aef5e0db067e624994827f9fc
Olavo <olavo.santos@skipthedishes.ca>
2020-11-07 17:35:51 -0600

    added documentation on storage types

--- src/app/storage/index.ts
+++ src/app/storage/index.ts
@@ -6,15 +6,39 @@ import { getCookieClient } from 'app/storage/CookieStorage/helpers/getCookieClie
 
 export class StorageServiceProvider extends ServiceProvider {
   register() {
+    /**
+     * In memory storage
+     * This type of storage is very ephemeral and gets lost upon
+     * page refresh.
+     */
     this.container.value('storage/memory', new StorageWrapper(new InMemoryStorage()));
-    this.container.value(
-      'storage/local',
-      new StorageWrapper(process.browser ? window.localStorage : new InMemoryStorage()),
-    );
+
+    /**
+     * Session storage
+     * This type of storage is persisted while the window is not
+     * closed. The data is kept in the client's browser if
+     * the tab is refreshed.
+     */
     this.container.value(
       'storage/session',
       new StorageWrapper(process.browser ? window.sessionStorage : new InMemoryStorage()),
     );
+
+    /**
+     * Local storage
+     * This type of storage is the most persistent one. This is persisted
+     * in the client's browser even after the window is closed.
+     */
+    this.container.value(
+      'storage/local',
+      new StorageWrapper(process.browser ? window.localStorage : new InMemoryStorage()),
+    );
+
+    /**
+     * Cookie storage
+     * Cookies are a useful way to traffic information between the client
+     * and the server.
+     */
     this.container.value('storage/cookie', new CookieStorageWrapper(getCookieClient()));
   }
 }
